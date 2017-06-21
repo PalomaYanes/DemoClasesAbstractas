c232e1f748303541f9e634599924d24c90b0f7c2
Paloma del Rosario Yanes Cach <16470313@itcampeche.edu.mx>
2017-06-20 22:17:44 -0500

    Version final del Proyecto.

--- src/Modelo/EmpleadoAsalariado.java
+++ src/Modelo/EmpleadoAsalariado.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Modelo;
 
 /**
  *
  * @author palom
  */
-public class EmpleadoAsalariado {
+public class EmpleadoAsalariado extends Empleado  {
+   
+private double salarioSemanal;
+
+public EmpleadoAsalariado  (String nombre,String apellido, String nss, 
+        double salario ){
     
+    super (nombre, apellido, nss);
+    establecerSalarioSemanal (salario);
 }
+public void establecerSalarioSemanal (double salario){
+    if (salario >= 0.0)
+        salarioSemanal=salario;
+    else 
+        throw new IllegalArgumentException (
+        "El salario semenal deber ser >= 0.0");
+}
+public double obtenerSalarioSemanal (){
+    return salarioSemanal;
+}
+@Override 
+public double ingresos (){
+    return obtenerSalarioSemanal();
+}
+@Override 
+public String toString (){
+    return String.format("empelado asalariado:%s/n%s:$%,.2f",
+            super.toString(),"salario semanal", obtenerSalarioSemanal()); 
+}
+}
+
+
--- src/Modelo/EmpleadoBaseMasComision.java
+++ src/Modelo/EmpleadoBaseMasComision.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Modelo;
 
 /**
  *
  * @author palom
  */
-public class EmpleadoBaseMasComision {
+public class EmpleadoBaseMasComision extends EmpleadoPorComision {
+    private double salarioBase;
     
+    public EmpleadoBaseMasComision (String nombre, String apellido, String nss,
+            double ventas, double tarifa, double salario){
+        
+        super (nombre,apellido,nss,ventas,tarifa);
+        establecerSalarioBase (salario);
+        
+    }
+    public void establecerSalarioBase (double salario){
+        if(salario >= 0.0)
+            salarioBase= salario;
+        else
+            throw new IllegalArgumentException ( 
+                    "El salario base debe ser >= 0.0");
+    }
+    public double obtenerSalarioBase (){
+        return salarioBase;
+    }
+    @Override
+    public double ingresos (){
+        return obtenerSalarioBase() + super.ingresos();
+    }
+    @Override
+    public String toString (){
+        return String.format("%s %s; %s: $%,.2f",
+                "con salario base", super.toString (),
+                "salario base", obtenerSalarioBase());
+    }
+            
 }
